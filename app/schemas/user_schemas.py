cd6fa2e91ab796c5e84895eff27647aa4eddaf09
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-06 23:18:44 +0530

    user schemas added

---
+++ app/schemas/user_schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr, field_validator
+from datetime import datetime
+
+class UserBase(BaseModel):
+    full_name: str
+    email: EmailStr
+    username: str
+    
+
+class CreateUser(UserBase):
+    password: str
+    confirm_password: str
+
+    @field_validator("confirm_password")
+    def passwords_match(cls, v, values):
+        if "password" in values.data and v != values.data["password"]:
+            raise ValueError("Passwords do not match")
+        return v
+
+class UserResponse(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
