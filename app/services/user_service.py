f1e5e3853b56461cefd53c68ab81d532c692f43c
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-09 23:10:59 +0530

    hashing

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,6 @@
 from app.schemas.user_schemas import CreateUser, UserResponse
 from sqlalchemy.orm import Session
-
+'''why this architecture for tje real production system'''
 '''
 What the service should receive 
 -> db: Session - so it can call the repository
@@ -9,4 +9,4 @@ What the service should return
 -> UserResponse (or UserRead) - a response schema, not ORM
 '''
 def register_user(db: Session, user_data: CreateUser) -> UserResponse:
-    
+    pass 
