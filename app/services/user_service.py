ac92ab0a6c2fce2e870d2a09f6383830c612633b
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-09 23:05:32 +0530

    hashing password

---
+++ app/services/user_service.py
@@ -0,0 +1,12 @@
+from app.schemas.user_schemas import CreateUser, UserResponse
+from sqlalchemy.orm import Session
+
+'''
+What the service should receive 
+-> db: Session - so it can call the repository
+-> user_data: CreateUser - schema from the API layer
+What the service should return 
+-> UserResponse (or UserRead) - a response schema, not ORM
+'''
+def register_user(db: Session, user_data: CreateUser) -> UserResponse:
+    
