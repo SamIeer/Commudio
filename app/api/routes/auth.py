2976932489d9aa5eb34f68d5087a50e6c04886a0
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-15 20:45:49 +0530

    Auth router

---
+++ app/api/routes/auth.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated
+
+from sqlalchemy.orm import Session
+from app.core.database import get_db 
+
+from app.schemas.user_schemas import CreateUser, UserResponse
+from app.services.user_service import register_user
+
+router = APIRouter(prefix="/auth")
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/register", response_model=UserResponse)
+def register_user_route(user_data: CreateUser,db: db_dependency):
+    try:
+        new_user = register_user(db,user_data)
+        return new_user
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Email already exist")
+                
