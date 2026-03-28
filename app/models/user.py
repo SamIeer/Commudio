960c5d72da09ff198d7c963f1b1a3cab5eb5c24d
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-28 23:59:18 +0530

    user model only

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from app.core.database import Base
 from  sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, func, Enum, Text
 from sqlalchemy.orm import relationship
+from app.models.recording import Recording
 
 class User(Base):
     __tablename__ = 'users'
@@ -13,15 +14,4 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now()) # DateTime -> defines the column type, server_default=func.now()-> database automatically inserts the current timestamp when a record is created
 
-class Recording(Base):
-    __tablename__ = 'recordings'
-    id = Column(Integer,primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), index=True)
-    status  = Column(Enum("processing", "complete", "failed", name="status_enum"), default="processing") # (processing / complete / failed)
-    transcript = Column(Text)
-    filler_word_count = Column(Integer)
-    words_per_minute = Column(Integer)
-    feedback_text = Column(Text)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-
-    user = relationship("User", back_populates="recordings")
+    recordings = relationship("Recording", back_populates="user")
