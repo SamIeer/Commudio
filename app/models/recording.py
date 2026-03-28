cadb2f82286f5991164b618fb42a2c599d40645c
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-28 23:59:47 +0530

    Recording model

---
+++ app/models/recording.py
@@ -0,0 +1,18 @@
+
+from app.core.database import Base
+from  sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, func, Enum, Text
+from sqlalchemy.orm import relationship
+from app.models.user import User
+
+class Recording(Base):
+    __tablename__ = 'recordings'
+    id = Column(Integer,primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+    status  = Column(Enum("processing", "completed", "failed", name="status_enum"), default="processing") # (processing / complete / failed)
+    transcript = Column(Text, nullable=True)
+    filler_word_count = Column(Integer, nullable=True)
+    words_per_minute = Column(Integer, nullable=True)
+    feedback_text = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    user = relationship("User", back_populates="recordings")
