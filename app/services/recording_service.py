4588e9d6fb58209475e8b7f9861296c8aff1d596
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-03-29 23:30:55 +0530

    recording service

---
+++ app/services/recording_service.py
@@ -0,0 +1,40 @@
+from fastapi import HTTPException, UploadFile
+import tempfile
+
+from sqlalchemy.orm import Session
+from datetime import timedelta
+
+from app.models.recording import Recording
+from app.repositories.recording_repository import create_recording
+
+# Dummy transcrpition function 
+def transcribe_audio(file_path: str)-> str:
+    return "transcribed text example"
+
+def create_recording_service(db:Session,user_id: int,file: UploadFile):
+    # checking if file is correct 
+    if not file:
+        raise HTTPException(status_code=400, detail="No file provided")
+    
+    if not file.filename.endswith((".mp3",".wav",".m4a")):
+        raise HTTPException(status_code=400, detail="Invalid file type")
+    
+    try: 
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".wav") as temp:
+            temp.write(file.file.read())
+            temp.flush()
+
+            transcribe = transcribe_audio(temp.name)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    new_recording = Recording(
+        user_id =user_id,
+        status = "",
+        transcript=transcribe
+    )
+
+    return new_recording
+    #if correct then we give the entrẏ of recording 
+    # then return the recordings 
+
