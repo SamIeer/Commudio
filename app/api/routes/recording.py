a67d9eb843aa8da98e274dc7bba6e775f8052fd8
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-04-03 22:55:51 +0530

    recording upload endpoint

--- app/api/routes/recording.py
+++ app/api/routes/recording.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter,UploadFile, Depends
+from fastapi import APIRouter,UploadFile, File, Depends
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+from app.models.user import User
 from app.core.security import get_current_user
 from app.models.recording import Recording
 
