6474d47e1ae7c04814e6fef2eb1837d4d43d52c2
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-04-03 22:32:23 +0530

    Backgrounds tasks

--- app/services/recording_service.py
+++ app/services/recording_service.py
@@ -1,43 +1,71 @@
-from fastapi import HTTPException, UploadFile
+import os 
+
+from fastapi import HTTPException, UploadFile, BackgroundTasks
 import tempfile
 
 from sqlalchemy.orm import Session
 from datetime import timedelta
 
 from app.models.recording import Recording
-from app.repositories.recording_repository import create_recording
+from app.repositories.recording_repository import create_recording, update_recording
 
 # Dummy transcrpition function 
 def transcribe_audio(file_path: str)-> str:
     return "transcribed text example"
+def get_fillers(transcribe:str)->int:
+    return -1
+def get_wpm(transcribe:str)->int:
+    return -1
 
-def create_recording_service(db:Session,user_id: int,file: UploadFile):
-    # checking if file is correct 
-    if not file:
-        raise HTTPException(status_code=400, detail="No file provided")
-    
-    if not file.filename.endswith((".mp3",".wav",".m4a")):
-        raise HTTPException(status_code=400, detail="Invalid file type")
-    
-    recording = create_recording(db, user_id)
-    try: 
-        with tempfile.NamedTemporaryFile(delete=True, suffix=".wav") as temp:
-            temp.write(file.file.read())
-            temp.flush()
+def process_recording(recording_id: int, file_path: str):
+    db = SessionLocal() # Create New Session
 
-            transcribe = transcribe_audio(temp.name)
+    try:  
+        transcript = transcribe_audio(file_path)
+        filler_word_count = get_fillers(transcript)
+        wpm = get_wpm(transcript)
+        
+        update_recording(
+            db,
+            recording_id,
+            transcript=transcript,
+            filler_word_count=filler_word_count,
+            words_per_minute=wpm,
+            status="completed"
+        )
 
-        recording.transcript = transcribe
-        recording.status = "complete"
-
-        db.commit()
-        db.refresh(recording)
-
-        return Recording
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
-    new_recording = create_recording(db,user_id,file)
+        print("Processinģerror:", e)
+        update_recording(
+            db,
+            recording_id,
+            status="Failed"
+        )
+    finally:
+        db.close()
 
-    return new_recording
+        # Important Cleanup
+        if os.exixts(file_path):
+            os.remove(file_path)
+
+
+async def create_recording_service(db:Session,user_id: int,file: UploadFile, background_tasks: BackgroundTasks):
+    # checking if file is correct 
+    recording = create_recording(db, user_id)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp:
+        temp.write(file.file.read())
+        temp_path = temp.name
+
+    background_tasks.add_task(
+        process_recording,
+        recording.id,
+        temp_path
+    )
+    
+    return {
+        "recording_id": recording.id,
+        "status": "processing"
+    }
+
     
