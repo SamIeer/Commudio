0e7475c7a0e2b6dd9e4c4974c05c520aa006111b
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-04-04 22:30:08 +0530

    endpoint validation

--- app/api/routes/recording.py
+++ app/api/routes/recording.py
@@ -1,40 +1,30 @@
-from fastapi import APIRouter,UploadFile, File, Depends, HTTPException
+from fastapi import BackgroundTasks,APIRouter,UploadFile, File, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
 from app.models.user import User
 from app.core.security import get_current_user
 from app.models.recording import Recording
+from app.services.recording_service import create_recording_service
 
 rrecording = APIRouter()
 
 MAX_FILE_SIZE = 25*1024*1024
 ALLOWED_TYPES = (".mp4",".wav", ".m4a")
 
-@rrecording.post("/recordings/")
+@rrecording.post("/recordings")
 async def upload_recording(
     file: UploadFile = File(...),
-    current_user: User = Depends(get_current_user), 
-    db: Session = Depends(get_db)):
-
-    # read file
-    content = await file.read()
-
-    if len(content) > MAX_FILE_SIZE:
-        raise HTTPException(status_code=400, detail="File too large")
-
-    # create recoding entry #
-    recording = Recording(
-        user_id = current_user.id,
-        status = "processing"
-    ) # set status'
-
-    return {
-        "filename": file.filename,
-        "content_type": file.content_type,
-        "size": len(content),
-        "user_id": current_user.id
-    }
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+    background_tasks: BackgroundTasks
+):
+    return await create_recording_service(
+        db=db,
+        user_id=current_user.id,
+        file=file,
+        background_tasks=background_tasks
+    )
 
 
 @router.get("/recordings/{recording_id}")
