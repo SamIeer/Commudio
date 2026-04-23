77efb15b3d1774709fe4013a9cb723a2c62e7d9c
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-04-23 20:59:24 +0530

    setting class

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,19 @@
+from pathlib import Path
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
-    DATABASE_URL: str
+BASE_DIR = Path(__file__).resolve().parent.parent  # → app/
+ENV_PATH = BASE_DIR / ".env"
 
-    class Config:
-        env_file = ".env"
+load_dotenv(ENV_PATH)
+
+class Settings(BaseSettings):
+    secret_key: str
+    database_url: str
+    api_key: str
+    algorithm: str = 'HS256'
+    
 
 settings = Settings()
-'''
--> it reads DATABASE_URL from DOCKER env
--> IT also eorks locally with .env
--> Centralized config (production pattern)
-'''
+
+
