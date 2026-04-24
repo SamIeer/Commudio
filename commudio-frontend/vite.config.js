4d4954807bb7378730469691238d44353ee6ac9a
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-04-24 20:53:08 +0530

    host permission

--- commudio-frontend/vite.config.js
+++ commudio-frontend/vite.config.js
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: true,
-//     port: 5173,
-//     watch: {
-//       usePolling: true
-//     }
-//   }
-// })
-
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['commudio.onrender.com']
+    port: 5173,
+    allowedHosts: ['commudio.onrender.com'],
+    watch: {
+      usePolling: true
+    }
   }
 })
+
